774b846de7a6fa703902717592d4c53d25ceec02
FaizulOsman <faizulosmanrabby@gmail.com>
2023-11-04 23:38:59 +0600

    feat: loader added in upload image section

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Loader from "./Loader";
 
 const ImageUpload = ({ handleUploadImage }) => {
   const [isLoading, setIsLoading] = useState(false);
@@ -43,26 +44,40 @@ const ImageUpload = ({ handleUploadImage }) => {
   return (
     <>
       {isLoading ? (
-        <div>Loading...</div>
+        <Loader />
       ) : (
         <form
+          className="image-upload__wrapper"
           method="post"
           onChange={handleOnChange}
           onSubmit={handleOnSubmit}
-          className="flex flex-col sm:flex-row gap-4 sm:justify-between"
         >
-          <input
-            type="file"
-            name="file"
-            className="file-input file-input-sm file-input-primary file-input-bordered w-full bg-transparent"
-            required
-          />
+          <div className="image-upload">
+            <svg
+              className="image-upload__icon"
+              stroke="currentColor"
+              fill="currentColor"
+              stroke-width="0"
+              viewBox="0 0 24 24"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="7.499" cy="9.5" r="1.5"></circle>
+              <path d="m10.499 14-1.5-2-3 4h12l-4.5-6z"></path>
+              <path d="M19.999 4h-16c-1.103 0-2 .897-2 2v12c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2zm-16 14V6h16l.002 12H3.999z"></path>
+            </svg>
+            <input
+              className="image-upload__input"
+              type="file"
+              name="file"
+              required
+            />
 
-          {/* {imageSrc && !uploadData && ( */}
-          <button type="submit" className="btn btn-sm btn-primary">
-            Upload
-          </button>
-          {/* )} */}
+            <button className="image-upload__submit-btn" type="submit">
+              Add Image
+            </button>
+          </div>
         </form>
       )}
     </>
---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader-wrapper">
+        <span class="loader"></span>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
