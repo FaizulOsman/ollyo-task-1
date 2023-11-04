ae61fd4059d1a7a11439195fd5f8ea3764721caa
FaizulOsman <faizulosmanrabby@gmail.com>
2023-11-04 23:56:58 +0600

    link: link with backend live

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -4,7 +4,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    // baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://ollyo-task-1-gallery-server.vercel.app/api/v1",
   }),
   tagTypes: ["gallery"],
   endpoints: () => ({}),
